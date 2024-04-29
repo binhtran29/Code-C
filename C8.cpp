aa4b1de45351d15872c9f01c31520d7fc7c0ae26
Mèo Ăn Đêm <52396338+binhtran29@users.noreply.github.com>
2024-04-30 00:54:18 +0700

    Complete C8

--- C8.cpp
+++ C8.cpp
@@ -15,9 +15,60 @@ void ip(int *n)
         }
 }
 
-void GenRand(int n, int **a)
+int *GenRand(int n)
 {
-    *a = (int*) malloc(sizeof(int) * n);
+    int *a = (int*) malloc(sizeof(int) * n);
     For(i,0,n)
         a[i] = rand() % 20001;
+    return a;
+}
+
+int *EachArr(int n, int *a, int *d)
+{
+    int *tmp = (int*) malloc(sizeof(int) * n);
+    tmp[0] = a[0];
+    *d = 1;
+    For(i,1,n)
+        if(a[i] != tmp[0])
+        {
+            tmp[*d] = a[i];
+            *d+=1;
+        }
+    return tmp;
+}
+
+int *Frequence(int n, int *a)
+{
+    int d;
+    int *tmp = EachArr(n,a,&d);
+    int *f = (int*) malloc(sizeof(int) * d);
+    For(i,0,d)
+        For(j,0,n)
+            if(a[j] == tmp[i])
+                f[i] += 1;
+    return f;
+}
+
+void op(int n, int *a)
+{
+    For(i,0,n)
+        printf("%6d",*(a+i));
+}
+
+int main()
+{
+    int n, *a, *tmp, *f, d;
+
+    ip(&n);
+
+    a = GenRand(n);
+
+    tmp = EachArr(n,a,&d);
+
+    f = Frequence(n,a);
+
+    printf("Tan suat xuat hien cac phan tu trong mang la:\n");
+    op(d,tmp);
+    printf("\n");
+    op(d,f);
 }
